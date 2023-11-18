c5e4fbad03fd74c1e5eabbd7481aee7fb34f94ba
Rahul Midha <rahulmidha@Rahul-Midha-MacBook-AirM2.local>
2023-11-19 00:07:26 +0530

    small env changes

--- server/app.js
+++ server/app.js
@@ -45,6 +45,7 @@ app.use("/api/v1/notification", notificationRoutes);
 //   errorLogger.error(err.message);
 // });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
